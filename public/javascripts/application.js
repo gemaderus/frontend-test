32a97eb0330212b7a7f67dc32d952a6ec263acf8
gemaderus <hi@gemaderus.com>
2016-10-18 12:30:21 +0200

    Layout integration.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -227,6 +227,74 @@
 	  {month: 12, views: 10}
 	], ".graphic4", "#BCA5D1");
 
+	//Graphic 5
+
+	drawStat([
+	  {month: 1, views: 3},
+	  {month: 2, views: 200},
+	  {month: 3, views: 80},
+	  {month: 4, views: 700},
+	  {month: 5, views: 300},
+	  {month: 6, views: 150},
+	  {month: 7, views: 600},
+	  {month: 8, views: 800},
+	  {month: 9, views: 340},
+	  {month: 10, views: 80},
+	  {month: 11, views: 200},
+	  {month: 12, views: 10}
+	], ".graphic5", "#D267C6");
+
+	//Graphic 6
+
+	drawStat([
+	  {month: 1, views: 10},
+	  {month: 2, views: 3000},
+	  {month: 3, views: 150},
+	  {month: 4, views: 20},
+	  {month: 5, views: 1000},
+	  {month: 6, views: 1200},
+	  {month: 7, views: 3000},
+	  {month: 8, views: 800},
+	  {month: 9, views: 340},
+	  {month: 10, views: 80},
+	  {month: 11, views: 200},
+	  {month: 12, views: 10}
+	], ".graphic6", "#EFAA42");
+
+	//Graphic 7
+
+	drawStat([
+	  {month: 1, views: 3},
+	  {month: 2, views: 1000},
+	  {month: 3, views: 2000},
+	  {month: 4, views: 700},
+	  {month: 5, views: 100},
+	  {month: 6, views: 1200},
+	  {month: 7, views: 600},
+	  {month: 8, views: 1800},
+	  {month: 9, views: 1040},
+	  {month: 10, views: 600},
+	  {month: 11, views: 1200},
+	  {month: 12, views: 10}
+	], ".graphic7", "#FB6533");
+
+	//Graphic 8
+
+	drawStat([
+	  {month: 1, views: 3},
+	  {month: 2, views: 800},
+	  {month: 3, views: 300},
+	  {month: 4, views: 100},
+	  {month: 5, views: 750},
+	  {month: 6, views: 500},
+	  {month: 7, views: 350},
+	  {month: 8, views: 800},
+	  {month: 9, views: 340},
+	  {month: 10, views: 80},
+	  {month: 11, views: 900},
+	  {month: 12, views: 10}
+	], ".graphic8", "#BCA5D1");
+
 	//Slider
 	//
 
